fe8afe96aba15158dc3f6abb017fb748b18084b5
alfredkam <akam@adcnx.com>
2015-04-02 18:44:05 -0700

    better debug

--- test.php
+++ test.php
@@ -1,5 +1,10 @@
 <?php
 $link = mysql_connect(getenv('DB_HOST') . ':' . getenv('DB_PORT'), getenv('DB_USER'), getenv('DB_PASSWORD'));
+
+echo getenv('DB_HOST') . ':' . getenv('DB_PORT');
+echo getenv('DB_USER');
+echo getenv('DB_PASSWORD');
+
 if (!$link) {
     die('Could not connect: ' . mysql_error());
 }
