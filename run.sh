62372de5afe04343baa05afa7f06d5226070dade
alfredkam <akam@adcnx.com>
2015-09-29 20:50:11 -0700

    remove new line

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
 # Add NewRelic keys
-echo "\nnewrelic.license=\"$NEWRELIC_LICENSE\"" >> /etc/php5/mods-available/newrelic.ini && \
-echo "\nnewrelic.appname=\"$NEWRELIC_APPNAME\"" >> /etc/php5/mods-available/newrelic.ini && \
+echo "newrelic.license=\"$NEWRELIC_LICENSE\"" >> /etc/php5/mods-available/newrelic.ini && \
+echo "newrelic.appname=\"$NEWRELIC_APPNAME\"" >> /etc/php5/mods-available/newrelic.ini && \
 cp /etc/php5/mods-available/newrelic.ini /etc/php5/cli/conf.d/newrelic.ini && \
 cp /etc/php5/mods-available/newrelic.ini /etc/php5/apache2/conf.d/newrelic.ini
 
