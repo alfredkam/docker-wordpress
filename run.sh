42ca0956afe597cf09f822ca9be19d233072b473
alfredkam <akam@adcnx.com>
2015-09-29 20:47:16 -0700

    Second attempt

--- run.sh
+++ run.sh
@@ -6,11 +6,12 @@
 read pid cmd state ppid pgrp session tty_nr tpgid rest < /proc/self/stat
 trap "kill -TERM -$pgrp; exit" EXIT TERM KILL SIGKILL SIGTERM SIGQUIT
 
-source /etc/apache2/envvars
-exec apache2 -D FOREGROUND
-
 # Add NewRelic keys
 echo "\nnewrelic.license=\"$NEWRELIC_LICENSE\"" >> /etc/php5/mods-available/newrelic.ini && \
 echo "\nnewrelic.appname=\"$NEWRELIC_APPNAME\"" >> /etc/php5/mods-available/newrelic.ini && \
 cp /etc/php5/mods-available/newrelic.ini /etc/php5/cli/conf.d/newrelic.ini && \
 cp /etc/php5/mods-available/newrelic.ini /etc/php5/apache2/conf.d/newrelic.ini
+
+# Start service
+source /etc/apache2/envvars
+exec apache2 -D FOREGROUND
